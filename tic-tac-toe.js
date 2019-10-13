d193110fc3685b49ecfccf083463f6ceec1fe9ed
toyapinnock <toyapinnock@gmail.com>
2019-10-12 20:28:58 -0500

    changes to js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-    document.getElementById('board').classList.add('square');
+    
+    var squares= document.getElementById("board")
+    var divs= squares.getElementsByTagName("div");
+    for ( var i=0; i<divs.length; i++){
+    divs[i].classList.add("square");
+
+    }
+
+
+   // document.querySelector('div').setAttribute('class', square);
+    
+    //var squares= document.getElementById("Board")
+    //var divsquare= squares.getElementsByTagName('div');
+    //console.log(divsquare);
+    //var stylesquares= squares.classList.add('square');
 };
